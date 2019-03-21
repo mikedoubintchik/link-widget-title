61e41d46d6fd856569de224a964ff5f23c689f61
edindino <edin.djakovac@gmail.com>
2019-03-21 14:01:40 +0100

    Develop (#1)
    * Began implementation of title linking functionality in Wordpress widgets
    
    * Completed link title functionality
    
    * update function definition documentation

--- admin/class-link-widget-title-admin.php
+++ admin/class-link-widget-title-admin.php
@@ -100,4 +100,117 @@ class Link_Widget_Title_Admin {
 
 	}
 
+	/**
+	 * Add Title Link field to widget form
+	 *
+	 * @uses add_action() 'in_widget_form'
+	 *
+	 * @param $widget
+	 * @param $args
+	 * @param $instance
+	 */
+  public function add_title_link_fields_to_widget_form( $widget, $args, $instance ) {
+  ?>
+    <fieldset class="title-link-options">
+      <p><label for="<?php echo $widget->get_field_id('title_link'); ?>"><?php _e('Title link <small class="description">(Example: http://google.com)</small>', 'widget-title-links'); ?></label>
+      <input type="text" name="<?php echo $widget->get_field_name('title_link'); ?>" id="<?php echo $widget->get_field_id('title_link'); ?>"" class="widefat" value="<?php echo $instance['title_link']; ?>"" /></p>
+      
+      <p>
+        <input type="checkbox" class="checkbox" id="<?php echo $widget->get_field_id('title_link_target_blank'); ?>" name="<?php echo $widget->get_field_name('title_link_target_blank'); ?>"<?php checked( $instance['title_link_target_blank'] ); ?> />
+        <label for="<?php echo $widget->get_field_id('title_link_target_blank'); ?>"><?php _e( 'Open link in new window/tab', 'widget-title-links' ); ?></label>
+      </p>
+      
+      <p>
+        <input type="checkbox" class="checkbox" id="<?php echo $widget->get_field_id('title_link_wrap'); ?>" name="<?php echo $widget->get_field_name('title_link_wrap'); ?>"<?php checked( $instance['title_link_wrap'] ); ?> />
+        <label for="<?php echo $widget->get_field_id('title_link_wrap'); ?>"><?php _e( 'Make the entire title bar clickable', 'widget-title-links' ); ?></label>
+      </p>
+    </fieldset>
+  <?php
+  }
+
+	/**
+	 * Register the additional widget field
+	 *
+	 * @uses add_filter() 'widget_form_callback'
+	 *
+	 * @param $instance
+	 * @param $widget
+	 *
+	 * @return mixed
+	 */
+  public function register_widget_title_link_field ( $instance, $widget ) {
+    if ( !isset($instance['title_link']) )
+      $instance['title_link'] = null;
+    if ( !isset($instance['title_link_target_blank']) )
+      $instance['title_link_target_blank'] = null;    
+    if ( !isset($instance['title_link_wrap']) )
+      $instance['title_link_wrap'] = null;    
+    return $instance;
+  }
+
+	/**
+	 * Add the additional field to widget update callback
+	 *
+	 * @uses add_filter() 'widget_update_callback'
+	 *
+	 * @param $instance
+	 * @param $new_instance
+	 *
+	 * @return mixed
+	 */
+  public function widget_update_extend ( $instance, $new_instance ) {
+    $instance['title_link'] = esc_url( $new_instance['title_link'] );
+    $instance['title_link_target_blank'] = !empty($new_instance['title_link_target_blank']) ? 1 : 0;
+    $instance['title_link_wrap'] = !empty($new_instance['title_link_wrap']) ? 1 : 0;
+    return $instance;
+  }
+
+	/**
+	 * Add link to widget title on output
+	 *
+	 * @uses add_filter() 'dynamic_sidebar_params'
+	 *
+	 * @param $params
+	 *
+	 * @return mixed
+	 */
+  public function add_link_to_widget_title( $params ) {
+    if (is_admin())
+      return $params;
+
+    global $wp_registered_widgets;
+    $id = $params[0]['widget_id'];
+
+    if (isset($wp_registered_widgets[$id]['callback'][0]) && is_object($wp_registered_widgets[$id]['callback'][0])) {
+      // Get settings for all widgets of this type
+      $settings = $wp_registered_widgets[$id]['callback'][0]->get_settings();
+
+      // Get settings for this instance of the widget
+      $instance = $settings[substr( $id, strrpos( $id, '-' ) + 1 )];
+
+      // Allow overriding the title link programmatically via filters
+      $link = isset($instance['title_link']) ? $instance['title_link'] : null;
+      $link = apply_filters('widget_title_link', $link, $instance);
+
+      if ( $link ) {
+        $target = $instance['title_link_target_blank'] ? ' target="_blank"' : '';
+
+        // Wrap everything before_title inside the link, if wrap mode is on
+        if ( isset($instance['title_link_wrap']) && $instance['title_link_wrap'] ) {
+          $params[0]['before_title'] = '<a href="' . $link . '"' . $target . '>' . $params[0]['before_title'];
+          $params[0]['after_title']  = $params[0]['after_title'] . '</a>';
+        }
+
+        // Otherwise, only wrap the actual title
+        else {
+          $params[0]['before_title'] = $params[0]['before_title'] . '<a href="' . $link . '"' . $target . '>';
+          $params[0]['after_title']  = '</a>' . $params[0]['after_title'];
+        }
+
+      }
+    }
+
+    return $params;
+  }
+
 }
